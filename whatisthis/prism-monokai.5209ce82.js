a805bb65094c8d3ab4eea405ff2cbef5c5116ada
Sam Roelants <roelants.sam@gmail.com>
2019-08-01 01:09:16 +0200

    Relative assets urls

--- whatisthis/prism-monokai.5209ce82.js
+++ whatisthis/prism-monokai.5209ce82.js
@@ -217,7 +217,7 @@ var parent = module.bundle.parent;
 if ((!parent || !parent.isParcelRequire) && typeof WebSocket !== 'undefined') {
   var hostname = "" || location.hostname;
   var protocol = location.protocol === 'https:' ? 'wss' : 'ws';
-  var ws = new WebSocket(protocol + '://' + hostname + ':' + "38317" + '/');
+  var ws = new WebSocket(protocol + '://' + hostname + ':' + "36971" + '/');
 
   ws.onmessage = function (event) {
     checkedAssets = {};
@@ -393,4 +393,4 @@ function hmrAcceptRun(bundle, id) {
   }
 }
 },{}]},{},["node_modules/parcel/src/builtins/hmr-runtime.js"], null)
-//# sourceMappingURL=/prism-monokai.5209ce82.js.map
+//# sourceMappingURL=prism-monokai.5209ce82.js.map
