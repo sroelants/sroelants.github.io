596e984d905266fdc13a884a890b90c269988296
Sam Roelants <roelants.sam@gmail.com>
2019-08-01 15:15:01 +0200

    responsive css

--- whatisthis/js.5c615dc2.js
+++ whatisthis/js.5c615dc2.js
@@ -1048,7 +1048,7 @@ var parent = module.bundle.parent;
 if ((!parent || !parent.isParcelRequire) && typeof WebSocket !== 'undefined') {
   var hostname = "" || location.hostname;
   var protocol = location.protocol === 'https:' ? 'wss' : 'ws';
-  var ws = new WebSocket(protocol + '://' + hostname + ':' + "42729" + '/');
+  var ws = new WebSocket(protocol + '://' + hostname + ':' + "38867" + '/');
 
   ws.onmessage = function (event) {
     checkedAssets = {};
