645e1df3f74e497be823f6e7dc693347a76a2d2f
Sam Roelants <roelants.sam@gmail.com>
2019-08-01 17:02:03 +0200

    responsive css

--- whatisthis/prism.f704eac5.js
+++ whatisthis/prism.f704eac5.js
@@ -923,7 +923,7 @@ var parent = module.bundle.parent;
 if ((!parent || !parent.isParcelRequire) && typeof WebSocket !== 'undefined') {
   var hostname = "" || location.hostname;
   var protocol = location.protocol === 'https:' ? 'wss' : 'ws';
-  var ws = new WebSocket(protocol + '://' + hostname + ':' + "34071" + '/');
+  var ws = new WebSocket(protocol + '://' + hostname + ':' + "40871" + '/');
 
   ws.onmessage = function (event) {
     checkedAssets = {};
